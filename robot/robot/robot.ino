0d693cf448ab3452cfad75e2b471bf33f63c4641
Corey Reichle <corey.reichle@gmail.com>
2015-02-19 19:12:02 -0500

    Found some bugs in the robot code

--- robot/robot/robot.ino
+++ robot/robot/robot.ino
@@ -1,4 +1,9 @@
 //* Semi-autonomous robot
+//* Code taken from a few places, ie the PING))) sensor from Parrallax's example code,
+//* particularly.
+//* All other code Copyright (c) 2015 by Corey Reichle.  Released under the GPL ver 3, where
+//* applicable.
+
 #include <MakeItRobotics.h>//include library
 #include <Servo.h> 
 
@@ -7,78 +12,70 @@ MakeItRobotics semiAuto;//declare object
 Servo fwdSensor;  // create servo object to control a servo 
 // a maximum of eight servo objects can be created
 
-// declare input pins
+// declare data pins
 int sonarPin = 5;
-
-// declare output pins
 int servoPin = 6;
 
 // declare constants
 int servoRight = 4;
 int servoLeft = 180;
 int servoCenter = 90;
+int turnSpeed  = 200;
+int driveSpeed = 500;
 
 // declare vars we want global
-long pingDistance;
-
 
 void setup() {
-  // initialize motors
-  Serial.begin(10420);                     //tell the Arduino to communicate with Make: it PCB
-  delay(500);                              //delay 500ms
-  semiAuto.all_stop();               //all motors stop
+                                                // initialize motors
+  Serial.begin(10420);                          //tell the Arduino to communicate with Make: it PCB
+  delay(500);                                   //delay 500ms
+  semiAuto.all_stop();                          //all motors stop
 
-    // initialize sensor
-  fwdSensor.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+                                                // initialize sensor
+  fwdSensor.attach(servoPin);                   // attaches the servo on pin 9 to the servo object
   fwdSensor.write(servoCenter);
 }
 
 void loop () {
   long distances[3];
 
-  //Drive forwards, until we hit prox alarm
-  while (pingCenter() > 1500){
-    semiAuto.go_forward(100);  
+                                                //Drive forwards, until we hit prox alarm
+  while (pingCenter() > 1000){
+    semiAuto.go_forward(driveSpeed);  
   }
-  //Stop, and backup a bit.
+                                                //Stop, and backup a bit.
   semiAuto.all_stop();
   delay(100);
-  semiAuto.go_backward(100);
+  semiAuto.go_backward(driveSpeed);
   delay(1000);
   semiAuto.all_stop();
 
-  //Scan
-  distances[0] = pingLeft();
-
-  distances[2] = pingRight();
-
-  // decide which way
-  // if left is better...
-  if ( distances[0] > ( distances[2] + 200 ) ) {
-    semiAuto.turn_left(100);
+                                                //Scan
+                                                
+  distances[1] = pingRight();
+  distances[2] = pingLeft();
+                                                // decide which way
+                                                // if left is better...
+  if ( distances[1] > distances[2] ) {
+    semiAuto.turn_right(turnSpeed);
     delay(500);
     semiAuto.all_stop();
   }
-  // else, go right...
-  else if (distances[2] > ( distances[0] + 200 ) ) {
-    semiAuto.turn_right(100);
+  else
+                                               // else, go right...
+    if (distances[2] > distances[1] ) {
+    semiAuto.turn_left(turnSpeed);
     delay(500);
     semiAuto.all_stop();
-  }
-  // if both suck, turn around.
-  else {
-    semiAuto.turn_right(100);
-    delay(1200);
-    semiAuto.all_stop(); 
   }
 
 }
 
 long pingLeft () {
   long  distance;
-  //turn the servo left
+                                               //turn the servo left
   fwdSensor.write(servoLeft);
-  //return the distance
+                                               //return the distance
   distance = ping();
   delay(200);
   return distance;
@@ -86,9 +83,9 @@ long pingLeft () {
 
 long pingCenter () {
   long  distance;
-  //Turn the servo center
+                                               //Turn the servo center
   fwdSensor.write(servoCenter);
-  //Return the ping time
+                                               //Return the ping time
   distance = ping();
   delay(200);
   return distance;
@@ -96,49 +93,26 @@ long pingCenter () {
 
 long pingRight () {
   long  distance;
-  //Turn the right
+                                               //Turn the right
   fwdSensor.write(servoRight);
-  //Return the ping time
+                                               //Return the ping time
   distance = ping();
   delay(200);
   return distance;
 }
 
 long ping () {
-  // Declare vars used internally to the function
   long duration;
-
+                                               // Send out ping
   pinMode(sonarPin, OUTPUT);
   digitalWrite(sonarPin, LOW);
   delayMicroseconds(2);
   digitalWrite(sonarPin, HIGH);
   delayMicroseconds(5);
   digitalWrite(sonarPin, LOW);
-
+                                               // Read ping duration
   pinMode(sonarPin, INPUT);
   duration = pulseIn(sonarPin, HIGH);
+                                               // Return the ping
   return duration;
 }
-
-long microsecondsToInches(long microseconds)
-{
-  // According to Parallax's datasheet for the PING))), there are
-  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-  // second).  This gives the distance travelled by the ping, outbound
-  // and return, so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-  return microseconds / 74 / 2;
-}
-
-long microsecondsToCentimeters(long microseconds)
-{
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
-  return microseconds / 29 / 2;
-}
-
-
-
-
-
